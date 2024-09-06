053c9301ead7605d85e345105f4b48c6df258e2c
ZephFzefe <fzefeweb@gmail.com>
2024-09-06 12:26:38 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Nintendo (NES)",
+        descricao: "O Nintendo Entertainment System (NES), mais conhecido como Nintendinho, foi um dos consoles de videogame mais populares e influentes da história.Lançado no final dos anos 80, o NES revolucionou a indústria dos games com gráficos coloridos, jogabilidade viciante e uma biblioteca de jogos memoráveis, como Super Mario Bros., The Legend of Zelda e Metroid.O NES não apenas marcou uma geração de jogadores, mas também consolidou a Nintendo como uma das maiores empresas de jogos eletrônicos do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Nintendo_Entertainment_System",
+        tags: "nintendinho mario samus link"
+    },
+    {
+        titulo: "Mega Drive",
+        descricao: "O Mega Drive, conhecido como Sega Genesis na América do Norte, foi um dos principais concorrentes do NES e SNES. Lançado no início dos anos 90, o Mega Drive se destacou por seus gráficos mais avançados e jogos com maior ênfase na ação e velocidade, como Sonic the Hedgehog e Streets of Rage.",
+        link: "https://pt.wikipedia.org/wiki/Mega_Drive",
+        tags: "master ouriço azul"
+    },
+    {
+        titulo: "PlayStation",
+        descricao: "O PlayStation foi o primeiro console de videogame da Sony e marcou a transição para a era dos jogos em 3D. Lançado em meados dos anos 90, o PlayStation revolucionou a indústria com gráficos tridimensionais impressionantes, jogos com histórias mais elaboradas e um controle analógico inovador. Títulos como Final Fantasy VII e Metal Gear Solid se tornaram clássicos do gênero.",
+        link: "https://pt.wikipedia.org/wiki/PlayStation",
+        tags: "cd crash analogico"
+    }
+];
+
